bb3497e4a06adb455bca531d5ffcd70176fdb1d0
omerblank <omerblank@gmail.com>
2022-12-23 15:30:34 +0200

    Adding readme. Adding program class for checks. Adding board class for the output.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static public void Main(String[] args)
+    {
+        Board check = new Board("100000027000304015500170683430962001900007256006810000040600030012043500058001000");
+        check.DrawBoard();
+    }
+}
+
