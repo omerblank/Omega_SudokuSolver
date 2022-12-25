11fd92c8b4a5ec9df26a9de1430905fe6e375fe5
omerblank <omerblank@gmail.com>
2022-12-25 22:38:14 +0200

    Changings in Output.cs.

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@
     static public void Main(String[] args)
     {
         Output check = new Output("100000027000304015500170683430962001900007256006810000040600030012043500058001000");
-        check.PreCalculation();
-
+        check.PrintBoard();
         Board mat = new Board("100000027000304015500170683430962001900007256006810000040600030012043500058001000");
         mat.FindOptions();
         
