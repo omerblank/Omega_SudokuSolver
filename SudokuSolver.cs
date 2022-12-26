4661d97555260e3da8e92dea860d252768c91002
omerblank <omerblank@gmail.com>
2022-12-26 19:24:23 +0200

    Checking execution time for the solving

--- SudokuSolver.cs
+++ SudokuSolver.cs
@@ -2,9 +2,14 @@
 {
     static public void Main(String[] args)
     {
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
         Output check = new Output("100000027000304015500170683430962001900007256006810000040600030012043500058001000");
         check.PrintBoard();
         check.Solve();
+        watch.Stop();
+        //right now the execution time is around 30 ms!
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
     }
 }
 
